c8b34e05f0ccc55691da2bb9a8630dc9acb71add
pqm <patrickqmurphy@gmail.com>
2019-12-17 21:10:21 -0600

    done

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,20 +1,19 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoicHFtdXJwaHkiLCJhIjoiY2szdGY4YTFtMDI5MzNkbDdkM3U5dzJjMiJ9.Vu8FS4DCrXwhSzfHNkWCxQ';
 var map = L.mapbox.map("map", 'mapbox.streets');
+
+// THIS NEEDS TO BE DELETED ONCE "pts" VAR         //
+// IS REPLACED BY THE DB AFFORDABLE HOUSE LAT LONS //
 var afhous = "Assets/Data/points.csv";
-var neighbounds = "Assets/Data/Neighborhoods.kml";
-var resultPoints;
-var csvFeatures;
-var polyFeatures;
+
+// KML DATA
+var neighbounds = "Assets/Data/Boundaries - Community Areas (current).kml";
 
 function filterPoints(csv, poly) {
     resultPoints = L.geoJson(turf.within(csv, poly));
     resultPoints.addTo(map);
 }
 
-// function coloritems(hoods, csv){
-
-// }
-
+// FUNCTION TO COLOUR EACH NEIGHBOURHOOD ///
 // function getColor(d) {
 //     return d > 1000 ? '#800026' :
 //            d > 500  ? '#BD0026' :
@@ -26,25 +25,73 @@ function filterPoints(csv, poly) {
 //                       '#FFEDA0';
 // }
 
-function omnivorize (csv, kml) {
-    var poly = omnivore.kml(kml)
-    .on('ready', function() {
-        polyFeatures = poly.toGeoJSON();
-    })
-    .addTo(map);
-    console.log(poly);
+// Styling and Hover Interaction
+function style(feature) {
+    return {
+        fillColor: 'blue',
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
 
-    var pts = omnivore.csv(csv)
-    .on('ready', function() {
-        map.fitBounds(pts.getBounds());
-        csvFeatures = pts.toGeoJSON();
-    })
-    .addTo(map);
-    
-    document.getElementById('button').onclick=function(){
-       map.removeLayer(pts);
-       filterPoints(csvFeatures, polyFeatures);
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
     }
 }
 
-omnivorize(afhous,neighbounds);
+function resetHighlight(e) {
+    poly.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    var hood = e.target.feature.properties.area_num_1;
+    map.fitBounds(e.target.getBounds());
+    /// PLACE REDRAW FUNCTION HERE ///
+    /// PLACE REDRAW FUNCTION HERE ///
+    /// PLACE REDRAW FUNCTION HERE ///
+    /// PLACE REDRAW FUNCTION HERE ///
+    /// PLACE REDRAW FUNCTION HERE ///
+    /// PLACE REDRAW FUNCTION HERE ///
+    /// PLACE REDRAW FUNCTION HERE ///
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+
+// Draw the map
+// Instantiate initial style //
+var customLayer = L.geoJson(null, {
+    style: style,
+    onEachFeature: onEachFeature
+});
+
+// Creates the GeoJSON Layers ///
+var poly = omnivore.kml(neighbounds, null, customLayer)
+poly.addTo(map);
+
+// Plots the Affordable Housing Points //
+var pts = omnivore.csv(afhous);
+
+pts.on('ready', function() {
+    map.fitBounds(pts.getBounds());
+})
+.addTo(map);
