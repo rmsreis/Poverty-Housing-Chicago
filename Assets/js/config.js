af23e41430c268601313d91a93cf2f876e57a398
pqm <patrickqmurphy@gmail.com>
2020-01-02 12:58:12 -0600

    done

--- Assets/js/config.js
+++ Assets/js/config.js
@@ -1,2 +1,313 @@
 // API key
 const API_KEY = "pk.eyJ1IjoicHFtdXJwaHkiLCJhIjoiY2szdGY4YTFtMDI5MzNkbDdkM3U5dzJjMiJ9.Vu8FS4DCrXwhSzfHNkWCxQ";
+
+var indexdat = [
+    {
+      "AreaNumber": 1,
+      "HARDSHIPINDEX": 39
+    },
+    {
+      "AreaNumber": 2,
+      "HARDSHIPINDEX": 46
+    },
+    {
+      "AreaNumber": 3,
+      "HARDSHIPINDEX": 20
+    },
+    {
+      "AreaNumber": 4,
+      "HARDSHIPINDEX": 17
+    },
+    {
+      "AreaNumber": 5,
+      "HARDSHIPINDEX": 6
+    },
+    {
+      "AreaNumber": 6,
+      "HARDSHIPINDEX": 5
+    },
+    {
+      "AreaNumber": 7,
+      "HARDSHIPINDEX": 2
+    },
+    {
+      "AreaNumber": 8,
+      "HARDSHIPINDEX": 1
+    },
+    {
+      "AreaNumber": 9,
+      "HARDSHIPINDEX": 8
+    },
+    {
+      "AreaNumber": 10,
+      "HARDSHIPINDEX": 21
+    },
+    {
+      "AreaNumber": 11,
+      "HARDSHIPINDEX": 25
+    },
+    {
+      "AreaNumber": 12,
+      "HARDSHIPINDEX": 11
+    },
+    {
+      "AreaNumber": 13,
+      "HARDSHIPINDEX": 33
+    },
+    {
+      "AreaNumber": 14,
+      "HARDSHIPINDEX": 53
+    },
+    {
+      "AreaNumber": 15,
+      "HARDSHIPINDEX": 35
+    },
+    {
+      "AreaNumber": 16,
+      "HARDSHIPINDEX": 34
+    },
+    {
+      "AreaNumber": 17,
+      "HARDSHIPINDEX": 28
+    },
+    {
+      "AreaNumber": 18,
+      "HARDSHIPINDEX": 50
+    },
+    {
+      "AreaNumber": 19,
+      "HARDSHIPINDEX": 70
+    },
+    {
+      "AreaNumber": 20,
+      "HARDSHIPINDEX": 71
+    },
+    {
+      "AreaNumber": 21,
+      "HARDSHIPINDEX": 42
+    },
+    {
+      "AreaNumber": 22,
+      "HARDSHIPINDEX": 23
+    },
+    {
+      "AreaNumber": 23,
+      "HARDSHIPINDEX": 85
+    },
+    {
+      "AreaNumber": 24,
+      "HARDSHIPINDEX": 10
+    },
+    {
+      "AreaNumber": 25,
+      "HARDSHIPINDEX": 73
+    },
+    {
+      "AreaNumber": 26,
+      "HARDSHIPINDEX": 92
+    },
+    {
+      "AreaNumber": 27,
+      "HARDSHIPINDEX": 83
+    },
+    {
+      "AreaNumber": 28,
+      "HARDSHIPINDEX": 15
+    },
+    {
+      "AreaNumber": 29,
+      "HARDSHIPINDEX": 87
+    },
+    {
+      "AreaNumber": 30,
+      "HARDSHIPINDEX": 96
+    },
+    {
+      "AreaNumber": 31,
+      "HARDSHIPINDEX": 76
+    },
+    {
+      "AreaNumber": 32,
+      "HARDSHIPINDEX": 3
+    },
+    {
+      "AreaNumber": 33,
+      "HARDSHIPINDEX": 7
+    },
+    {
+      "AreaNumber": 34,
+      "HARDSHIPINDEX": 82
+    },
+    {
+      "AreaNumber": 35,
+      "HARDSHIPINDEX": 47
+    },
+    {
+      "AreaNumber": 36,
+      "HARDSHIPINDEX": 78
+    },
+    {
+      "AreaNumber": 37,
+      "HARDSHIPINDEX": 97
+    },
+    {
+      "AreaNumber": 38,
+      "HARDSHIPINDEX": 57
+    },
+    {
+      "AreaNumber": 39,
+      "HARDSHIPINDEX": 26
+    },
+    {
+      "AreaNumber": 40,
+      "HARDSHIPINDEX": 88
+    },
+    {
+      "AreaNumber": 41,
+      "HARDSHIPINDEX": 14
+    },
+    {
+      "AreaNumber": 42,
+      "HARDSHIPINDEX": 58
+    },
+    {
+      "AreaNumber": 43,
+      "HARDSHIPINDEX": 55
+    },
+    {
+      "AreaNumber": 44,
+      "HARDSHIPINDEX": 60
+    },
+    {
+      "AreaNumber": 45,
+      "HARDSHIPINDEX": 41
+    },
+    {
+      "AreaNumber": 46,
+      "HARDSHIPINDEX": 75
+    },
+    {
+      "AreaNumber": 47,
+      "HARDSHIPINDEX": 79
+    },
+    {
+      "AreaNumber": 48,
+      "HARDSHIPINDEX": 38
+    },
+    {
+      "AreaNumber": 49,
+      "HARDSHIPINDEX": 52
+    },
+    {
+      "AreaNumber": 50,
+      "HARDSHIPINDEX": 51
+    },
+    {
+      "AreaNumber": 51,
+      "HARDSHIPINDEX": 65
+    },
+    {
+      "AreaNumber": 52,
+      "HARDSHIPINDEX": 64
+    },
+    {
+      "AreaNumber": 53,
+      "HARDSHIPINDEX": 62
+    },
+    {
+      "AreaNumber": 54,
+      "HARDSHIPINDEX": 98
+    },
+    {
+      "AreaNumber": 55,
+      "HARDSHIPINDEX": 44
+    },
+    {
+      "AreaNumber": 56,
+      "HARDSHIPINDEX": 32
+    },
+    {
+      "AreaNumber": 57,
+      "HARDSHIPINDEX": 67
+    },
+    {
+      "AreaNumber": 58,
+      "HARDSHIPINDEX": 84
+    },
+    {
+      "AreaNumber": 59,
+      "HARDSHIPINDEX": 61
+    },
+    {
+      "AreaNumber": 60,
+      "HARDSHIPINDEX": 43
+    },
+    {
+      "AreaNumber": 61,
+      "HARDSHIPINDEX": 91
+    },
+    {
+      "AreaNumber": 62,
+      "HARDSHIPINDEX": 69
+    },
+    {
+      "AreaNumber": 63,
+      "HARDSHIPINDEX": 93
+    },
+    {
+      "AreaNumber": 64,
+      "HARDSHIPINDEX": 29
+    },
+    {
+      "AreaNumber": 65,
+      "HARDSHIPINDEX": 56
+    },
+    {
+      "AreaNumber": 66,
+      "HARDSHIPINDEX": 80
+    },
+    {
+      "AreaNumber": 67,
+      "HARDSHIPINDEX": 89
+    },
+    {
+      "AreaNumber": 68,
+      "HARDSHIPINDEX": 94
+    },
+    {
+      "AreaNumber": 69,
+      "HARDSHIPINDEX": 66
+    },
+    {
+      "AreaNumber": 70,
+      "HARDSHIPINDEX": 37
+    },
+    {
+      "AreaNumber": 71,
+      "HARDSHIPINDEX": 74
+    },
+    {
+      "AreaNumber": 72,
+      "HARDSHIPINDEX": 12
+    },
+    {
+      "AreaNumber": 73,
+      "HARDSHIPINDEX": 48
+    },
+    {
+      "AreaNumber": 74,
+      "HARDSHIPINDEX": 16
+    },
+    {
+      "AreaNumber": 75,
+      "HARDSHIPINDEX": 30
+    },
+    {
+      "AreaNumber": 76,
+      "HARDSHIPINDEX": 24
+    },
+    {
+      "AreaNumber": 77,
+      "HARDSHIPINDEX": 19
+    }
+  ]
--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,34 +1,41 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoicHFtdXJwaHkiLCJhIjoiY2szdGY4YTFtMDI5MzNkbDdkM3U5dzJjMiJ9.Vu8FS4DCrXwhSzfHNkWCxQ';
 var map = L.mapbox.map("map", 'mapbox.streets');
-
-// THIS NEEDS TO BE DELETED ONCE "pts" VAR         //
-// IS REPLACED BY THE DB AFFORDABLE HOUSE LAT LONS //
 var afhous = "Assets/Data/points.csv";
-
+var zoomed = 0;
+console.log(zoomed+' i started zoomed out')
 // KML DATA
 var neighbounds = "Assets/Data/Boundaries - Community Areas (current).kml";
 
-function filterPoints(csv, poly) {
-    resultPoints = L.geoJson(turf.within(csv, poly));
-    resultPoints.addTo(map);
-}
+/// FUNCTION TO COLOUR EACH NEIGHBOURHOOD ///
+function getColor(id) {
 
-// FUNCTION TO COLOUR EACH NEIGHBOURHOOD ///
-// function getColor(d) {
-//     return d > 1000 ? '#800026' :
-//            d > 500  ? '#BD0026' :
-//            d > 200  ? '#E31A1C' :
-//            d > 100  ? '#FC4E2A' :
-//            d > 50   ? '#FD8D3C' :
-//            d > 20   ? '#FEB24C' :
-//            d > 10   ? '#FED976' :
-//                       '#FFEDA0';
-// }
+    var d;
+
+    // Go Through indexdat JSON object to grab hardship index of neighbourhood
+    indexdat.forEach(function(data) {
+        if (data.AreaNumber == id) {
+
+            d = data.HARDSHIPINDEX
+
+        }
+    })
+
+
+    return d > 90 ? '#800026' :
+           d > 80  ? '#BD0026' :
+           d > 70  ? '#E31A1C' :
+           d > 60  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 40   ? '#FEB24C' :
+           d > 30   ? '#FED976' :
+           d > 20   ? '#FFEDA0' :
+                      '#fff9e2' ;
+}
 
 // Styling and Hover Interaction
 function style(feature) {
     return {
-        fillColor: 'blue',
+        fillColor: getColor(feature.properties.area_num_1),
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -57,15 +64,24 @@ function resetHighlight(e) {
 }
 
 function zoomToFeature(e) {
-    var hood = e.target.feature.properties.area_num_1;
-    map.fitBounds(e.target.getBounds());
-    /// PLACE REDRAW FUNCTION HERE ///
-    /// PLACE REDRAW FUNCTION HERE ///
-    /// PLACE REDRAW FUNCTION HERE ///
-    /// PLACE REDRAW FUNCTION HERE ///
-    /// PLACE REDRAW FUNCTION HERE ///
-    /// PLACE REDRAW FUNCTION HERE ///
-    /// PLACE REDRAW FUNCTION HERE ///
+    if (zoomed == 0) {
+        // "hood" variable is used to indicate what the spider plot should draw //
+        var hood = e.target.feature.properties.area_num_1;
+        zoomed = 1;
+        map.fitBounds(e.target.getBounds());
+        /// PLACE REDRAW FUNCTION HERE ///
+        /// Redraw to neighbourhood ID ///
+        /// PLACE REDRAW FUNCTION HERE ///
+    }
+    else {
+        // "hood" variable is used to indicate what the spider plot should draw //
+        var hood = 0;
+        zoomed = 0;
+        map.fitBounds(pts.getBounds());
+        /// PLACE REDRAW FUNCTION HERE ///
+        /// Redraw to entire city HERE ///
+        /// PLACE REDRAW FUNCTION HERE ///
+    }
 }
 
 function onEachFeature(feature, layer) {
